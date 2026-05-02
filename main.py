d529c7c87d2ccfde52c78b2f5156134d68d942d4
gizmo6663-dev <gizmo6663@gmail.com>
2026-05-02 21:09:48 +0200

    Adjust background position hint in main.py

--- main.py
+++ main.py
@@ -1467,7 +1467,7 @@ try:
                     allow_stretch=True,
                     keep_ratio=True,
                     size_hint=(1.1, 1.1),
-                    pos_hint={'center_x': 0.5, 'center_y': 0.6},
+                    pos_hint={'center_x': 0.5, 'center_y': 0.3},
                     opacity=0.45)
                 content_wrap.add_widget(self._content_bg)
             self.content = RBox(bg_color=[BG2[0], BG2[1], BG2[2], 0.78],
